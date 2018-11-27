698531dcdc5cee08cf930105f75913fbd6e30646
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2018-11-27 15:10:02 +0100

    fix

--- sem.sh
+++ sem.sh
@@ -2,7 +2,7 @@ mkdir ../cache
 git status
 rm Cargo.lock
 ls -a
-#./src/ci/init_repo.sh . ../cache
-git submodule sync
-git submodule update -j 16 --init --recursive
-ls -a -R src/tools/clippy
+./src/ci/init_repo.sh . ../cache
+#git submodule sync
+#git submodule update -j 16 --init --recursive
+#ls -a -R src/tools/clippy
