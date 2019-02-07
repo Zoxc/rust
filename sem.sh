b11099a3c44eed70795378525c88c8a9063db225
John Kåre Alsaker <john.kare.alsaker@gmail.com>
2019-02-07 22:04:02 +0100

    test

--- sem.sh
+++ sem.sh
@@ -1,5 +1,5 @@
-mkdir ../cache
-./src/ci/init_repo.sh . ../cache
+mkdir ../cache_Dummy
+./src/ci/init_repo.sh . ../cache_Dummy
 #git submodule sync
 #git submodule update -j 16 --init --recursive
 #ls -a -R src/tools/clippy
